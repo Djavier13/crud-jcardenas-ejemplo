3d79409719c6e2c7f140a8d0f0dc615b8f675bf0
Javier Cárdenas Silva <javiercs893@gmail.com>
2020-04-22 21:42:34 -0500

    Agregado de os.environ

--- application.py
+++ application.py
@@ -1,10 +1,11 @@
+import os
 from flask import Flask, render_template
 app = Flask(__name__)
 
-
+nombre = os.environ['nombre']
 @app.route('/')
 def index():
-    return 'hola mundooo'
+    return 'hola mundooo desde mi maquina'
 
 if __name__ == '__main__':
   app.run(port=80, debug=False)
